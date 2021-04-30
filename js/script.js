cab4593cb93b9b00d89e9d4ef7d896713b5fd62a
Ivan Bulavenko <bulavenkoivan@gmail.com>
2021-04-30 14:52:49 +0300

    hwend

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 import galleryItems from '../gallery-items.js';
 
-const gallery = document.querySelector('.js-gallery'),
-lightboxEl = document.querySelector('.js-lightbox'),
-closeModalBtn = document.querySelector("button[data-action='close-lightbox']"),
-lightboxImgEl = document.querySelector('.lightbox__image'),
-overlay = document.querySelector(".lightbox__overlay"),
-itemsMarkup = createGalleryItem(galleryItems);
+const gallery = document.querySelector('.js-gallery');
+const lightboxEl = document.querySelector('.js-lightbox');
+const closeModalBtn = document.querySelector("button[data-action='close-lightbox']");
+const lightboxImgEl = document.querySelector('.lightbox__image');
+const overlay = document.querySelector(".lightbox__overlay");
+const itemsMarkup = createGalleryItem(galleryItems);
+let currentIndex;
 
-let currentImg;
 gallery.insertAdjacentHTML("beforeend", itemsMarkup);
-gallery.addEventListener("click", onImgClick);
-closeModalBtn.addEventListener("click", closeModal);
-overlay.addEventListener("click", () => closeModal());
+
+
 
 // функция создания разметки
 function createGalleryItem(galleryItems) {
@@ -31,44 +30,49 @@ function createGalleryItem(galleryItems) {
                 </a>
             </li>
     `}).join('');
-}
+};
 // функция фильтра кликов делегирования
 function onImgClick(evt) {
+    evt.preventDefault();
     if (evt.target.nodeName !== "IMG") {
         return;
     }
-    currentImg = evt.target;
-
-  evt.preventDefault();
-  addLightboxContent(currentImg);
-  onOpenModal();
-}
+  
+    let imageRef = evt.target;
+    let largeImageURL = imageRef.dataset.source;
+    lightboxImgEl.src = largeImageURL;
+    currentIndex = +imageRef.dataset.index;
+    onOpenModal();
+};
 // функиция открытия модалки
 
 function onOpenModal() {
     lightboxEl.classList.add('is-open');
-    document.addEventListener("keydown", onEscKeyPress);
-    document.removeEventListener("keyup", imageArrowsFlipping);
-}
+    window.addEventListener("keydown", onEscKeyPress);
+    overlay.addEventListener("click", onOverlayClick);
+};
 // функция закрытия модалки
 function closeModal() {
     lightboxEl.classList.remove('is-open');
-    document.removeEventListener("keydown", onEscKeyPress);
-    document.removeEventListener("keyup", imageArrowsFlipping);
-}
-// функция добавления url изображения в модалку
+    lightboxImgEl.src = '#';
+    window.removeEventListener("keydown", onEscKeyPress);
+    overlay.removeEventListener("click", onOverlayClick);
+};
 
-function addLightboxContent(url) {
-  if (lightboxImgEl.src !== "") {
-    lightboxImgEl.src = "";
-  }
-  lightboxImgEl.src = url.dataset.source;
-  lightboxImgEl.alt = url.alt;
-}
+
 // функция закрытия по ESC
+const onOverlayClick = evt =>
+  evt.currentTarget === evt.target ? closeModal() : '';
+    
 function onEscKeyPress(evt) {
   const ESC_KEY_CODE = "Escape";
   if (evt.code === ESC_KEY_CODE) {
     closeModal();
   }
-}
+  };
+
+
+gallery.addEventListener("click", onImgClick);
+closeModalBtn.addEventListener("click", closeModal);
+
+
